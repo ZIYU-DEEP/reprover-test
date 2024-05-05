e45973b8040fded5e5d0e49f5216e2f9a532f21f
Ziyu <ziyuye@uchicago.edu>
2024-05-05 18:05:56 -0400

    check bug in lean

--- bash/run.sh
+++ bash/run.sh
@@ -1,1 +1,10 @@
 CUDA_VISIBLE_DEVICES=3 python generator/main.py fit --config generator/confs/cli_lean4_random_modified.yaml  
+python generator/main.py fit --config generator/confs/cli_lean4_random_modified.yaml  
+
+python prover/evaluate.py \
+    --data-path data/leandojo_benchmark_4/random/ \
+    --ckpt_path leandojo-pl-ckpts/generator_random.ckpt \
+    --split test \
+    --num-workers 5 \
+    --num-gpus 1 \
+    --num-theorems 1
