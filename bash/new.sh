539c22fed22bb86ef5ea9b25fcd6a7beb67cb5d1
Ziyu <ziyuye@uchicago.edu>
2024-05-10 19:37:21 -0400

    add bash

---
+++ bash/new.sh
@@ -0,0 +1,19 @@
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_joint.yaml
+
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_goal.yaml
+
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_goal_driven_tactic.yaml
+
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_joint_ckpt.yaml
+
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_goal_ckpt.yaml
+
+python generator/main.py fit \
+    --config generator/confs/cli_lean4_random_goal_driven_tactic_ckpt.yaml
+
+# salloc --gres=gpu:A100:1 --mem=80G --ntasks-per-node=1 --account=gts-czhang355 -q inferno -t 24:00:00 -C A100-80GB
