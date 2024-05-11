70bc722da0d7ab78852bb3c52e2ea748f15f7068
Ziyu <ziyuye@uchicago.edu>
2024-05-11 21:20:43 +0000

    from ubuntu and have main the same

--- generator/main.py
+++ generator/main.py
@@ -14,14 +14,20 @@ class CustomCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         super().add_arguments_to_parser(parser)
         
+        # ------------------------------------------------------------------
         # New arguments to load a pretrained checkpoint weights
         parser.add_argument("--init_ckpt_path", type=str, default=None,
                             help="Path to the checkpoint file to init the model.")
-           
+        parser.add_argument("--init_ckpt_filename", type=str, default=None,
+                            help="Filename of the checkpoint to init the model.")
+        # ------------------------------------------------------------------
+         
+        # ------------------------------------------------------------------
         # Linking arguments
         parser.link_arguments("model.model_name", "data.model_name")
         parser.link_arguments("data.max_inp_seq_len", "model.max_inp_seq_len")
         parser.link_arguments("data.max_oup_seq_len", "model.max_oup_seq_len")
+         # ------------------------------------------------------------------
 
     def before_fit(self):
     
@@ -29,30 +35,12 @@ class CustomCLI(LightningCLI):
         # Initializing with trained weights
         logger.info(f'Config: {self.config}')
         init_ckpt_path = self.config.fit.init_ckpt_path
+        init_ckpt_filename = self.config.fit.init_ckpt_filename
         
         if init_ckpt_path:
-            # Set the device
-            device = self.config.fit.trainer.devices
-            if isinstance(device, list): device = f'cuda:{device[0]}' 
-            elif device: device = 'cuda'
-            else: device = 'cpu' 
-
-            # Load the model
-            # self.model = RetrievalAugmentedGenerator.load(
-            #     ckpt_path=init_ckpt_path,
-            #     device=device,
-            #     freeze=False
-            # )
-            ckpt_file = os.path.join(init_ckpt_path, 
-                                     "checkpoint/mp_rank_00_model_states.pt")
+            ckpt_file = os.path.join(init_ckpt_path, init_ckpt_filename)
             ckpt_module = torch.load(ckpt_file)['module']
-            self.model.generator.load_state_dict(ckpt_module,
-                                                 strict=False)
-            breakpoint()
-            # self.model.generator.load_state_dict(
-            #     torch.load(init_ckpt_path)['state_dict'],
-            #     strict=False,
-            # )
+            self.model.generator.load_state_dict(ckpt_module, strict=False)
             logger.info(f"Model loaded from checkpoint: {init_ckpt_path}")
         else:
             logger.info("No checkpoint provided; training from scratch.")
@@ -78,11 +66,15 @@ class CustomCLI(LightningCLI):
         
         
 def main():
+    
+     # ------------------------------------------------------------------
     # Set the logger
     set_logger(verbose=True)
     logger.info(f"PID: {os.getpid()}.")
     logger.info(f"Starting the training process.")
+     # ------------------------------------------------------------------
     
+     # ------------------------------------------------------------------
     # Set the client and train
     cli = CustomCLI(model_class=RetrievalAugmentedGenerator, 
                     datamodule_class=GeneratorDataModule,
@@ -90,6 +82,7 @@ def main():
                     run=True)
     
     logger.info(f"Configuration loaded and training is complete.")
+     # ------------------------------------------------------------------
 
 
 if __name__ == "__main__":
